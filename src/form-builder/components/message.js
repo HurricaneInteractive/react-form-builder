06ae1fab974c3c8ed38c1f1f17db74409ea5a6c8
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-11-22 17:41:37 +1100

    Conditional rendering based on type of html

--- src/form-builder/components/message.js
+++ src/form-builder/components/message.js
@@ -3,7 +3,18 @@ import React from "react"
 const Message = (props) => {
   const { html, ...attrs } = props
 
-  return <p dangerouslySetInnerHTML={{ __html: html }} {...attrs} />
+  if (typeof html === "string") {
+    return <p dangerouslySetInnerHTML={{ __html: html }} {...attrs} />
+  }
+  else if (typeof html === "function") {
+    const Component = html
+    return <Component {...attrs} />
+  }
+  else if (React.isValidElement(html)) {
+    return html
+  }
+
+  return <></>
 }
 
 export default Message
